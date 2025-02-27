b354d6e4fcc68d737c99cbb8c5146b8b26ef45cd
Aleksandar Milenkovic <milenkovicaleksandar470@gmail.com>
2025-02-27 13:50:01 +0100

    add gson

--- java/test/Runner.java
+++ java/test/Runner.java
@@ -1,14 +1,25 @@
 package org.example;
 
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.function.Function;
 
 import org.json.*;
+
+import com.google.gson.Gson;
+import com.google.gson.Gson.*;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.example.Struct;
 
 public class Main {
 
@@ -36,6 +47,8 @@ public class Main {
 
 
         JSONObject alltests = new JSONObject(fileContent);
+
+        System.out.println(alltests);
 
         JSONObject spec = alltests.has("primary") && ((JSONObject)alltests.get("primary")).has(name)
                 ? (JSONObject) ((JSONObject)alltests.get("primary")).get(name)
@@ -67,11 +80,64 @@ public class Main {
         Object subject = utility.get(name);
 
         Function<RunSetArguments, Void> runset = (func_args) -> {
-            JSONObject testspec_set = (JSONObject) func_args.testspec.get("set");
+            // System.out.println(func_args.testspec.getJSONArray("set"));
+
+            JSONArray testspec_set = func_args.testspec.getJSONArray("set");
             Object testsubject = func_args.testsubject;
 
-            Iterator<String> keys = testspec_set.keys();
+            // Iterator<String> keys = testspec_set.keys();
+            for(int i = 0; i < testspec_set.length(); i++) {
+                JSONObject entry = (JSONObject) testspec_set.get(i);
 
+                Object entry_in = entry.get("in");
+
+                // System.out.println(Struct.isNode(entry_in));
+                switch(entry_in) {
+                    case String s -> {
+                        System.out.println(Struct.isNode(s));
+                    }
+                    case JSONArray arr -> {
+                        System.out.println(Struct.isNode(arr.toList()));
+                    }
+                    case JSONObject obj -> {
+                        System.out.println(Struct.isNode(obj.toMap()));
+                    }
+                    case Integer integer -> {
+                        System.out.println(Struct.isNode(integer));
+                    }
+                    case Boolean bool -> {
+                        System.out.println(Struct.isNode(bool));
+                    }
+                    default -> {
+                        throw new AssertionError("Unknown entry in " + entry_in.getClass());
+                    }
+                }
+
+
+
+                /*
+
+                switch(entry_in) {
+                    case String s -> System.out.println("String: '" + s + "'");
+                    case JSONArray arr -> System.out.println("JSONArray: '" + arr.toList() + "'");
+                    case JSONObject obj -> System.out.println("JSONObject: '" + obj.toMap() + "'");
+                    default -> {
+                        System.out.println("Unknown entry: " + entry_in.getClass());
+                    }
+                }
+                 */
+
+                /*
+                System.out.println("entry: " + entry);
+                System.out.println("entry_in: " + entry_in);
+
+                 */
+
+
+
+            }
+
+            /*
             for (Iterator<String> it = keys; it.hasNext(); ) {
                 String key = it.next();
 
@@ -103,6 +169,7 @@ public class Main {
                     throw new AssertionError("Test failed: " + e.getMessage());
                 }
             }
+             */
             return null;
         };
 
@@ -166,6 +233,57 @@ public class Main {
                 Object testsubject = null;
 
                 runner_result.runset.apply(new RunSetArguments(isnode, testsubject));
+
+                {
+                    String jsonString = "{ \"name\": \"Alice\", \"age\": 25 }";
+
+//                    jsonString = "[]";
+//                    jsonString = "1";
+//                    jsonString = "a";
+
+
+                    Gson gson = new Gson();
+
+                    // Parse JSON string
+                    JsonElement jsonElement = JsonParser.parseString(jsonString);
+                    Object obj = gson.fromJson(jsonElement, Object.class);
+
+                    System.out.println(Struct.isNode(obj));
+
+                    switch(obj) {
+                        case Map map -> {
+                            System.out.println("Java Map: " + map.toString());
+                            break;
+                        }
+                        case List list -> {
+                            System.out.println("Java List: " + list.toString());
+                            break;
+                        }
+                        case Double _double -> {
+                            System.out.println("Java Double: " + _double.toString());
+                            break;
+                        }
+                        case String string -> {
+                            System.out.println("Java String: " + string);
+                            break;
+                        }
+                        default -> {
+                            System.out.println("Unmatched: " + obj.getClass());
+                        }
+                    }
+
+                    /*
+                    switch(jsonElement) {
+                        case JsonObject obj-> {
+                            System.out.println(obj.toString());
+                            break;
+                        }
+                        default -> {
+                            break;
+                        }
+                    }
+                     */
+                }
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
