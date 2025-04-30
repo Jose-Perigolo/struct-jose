a3509e12242c30f801e0c961d508b753e0ba40ac
Leonardo Cunha <agile.ice4816@fastmail.com>
2025-04-30 18:49:59 -0300

    remove unnecessary comments

--- lua/test/client_test.lua
+++ lua/test/client_test.lua
@@ -4,26 +4,16 @@
   Run with: busted client_test.lua
 ]]
 
--- Update package.path to include the current directory for module loading
 package.path = package.path .. ";./test/?.lua"
 
-local assert = require("luassert")
-
--- Import the runner module
 local runnerModule = require("runner")
 local makeRunner = runnerModule.makeRunner
 
--- Import the SDK module
 local SDK = require("sdk")
 
 local TEST_JSON_FILE = "../build/test/test.json"
 
-----------------------------------------------------------
--- Client Tests
-----------------------------------------------------------
-
 describe('client', function()
-  -- This test matches the TypeScript implementation in client.test.ts
   local runner = makeRunner(TEST_JSON_FILE, SDK:test())
   local runnerCheck = runner('check')
   local spec, runset, subject = runnerCheck.spec, runnerCheck.runset, runnerCheck.subject
