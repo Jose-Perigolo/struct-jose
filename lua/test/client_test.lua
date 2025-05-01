ee9fe2463a4ed2855e2340159c628fe04f27af89
Leonardo Cunha <agile.ice4816@fastmail.com>
2025-05-01 16:55:32 -0300

    refactor client test

--- lua/test/client_test.lua
+++ lua/test/client_test.lua
@@ -9,8 +9,10 @@ local TEST_JSON_FILE = "../build/test/test.json"
 
 describe('client', function()
   local runner = makeRunner(TEST_JSON_FILE, SDK:test())
+
   local runnerCheck = runner('check')
-  local spec, runset, subject = runnerCheck.spec, runnerCheck.runset, runnerCheck.subject
+  local spec, runset, subject = runnerCheck.spec, runnerCheck.runset,
+      runnerCheck.subject
 
   test('client-check-basic', function()
     runset(spec.basic, subject)
