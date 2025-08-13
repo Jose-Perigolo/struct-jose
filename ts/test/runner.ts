cd6dbecc2816795924632c68a16241be92800106
Richard Rodger <richard@ricebridge.com>
2025-08-13 14:07:07 +0100

    v0.0.7

--- ts/test/runner.ts
+++ ts/test/runner.ts
@@ -1,4 +1,4 @@
-// VERSION: @voxgig/struct 0.0.6
+// VERSION: @voxgig/struct 0.0.7
 // This test utility runs the JSON-specified tests in build/test/test.json.
 // (or .sdk/test/test.json if used in a @voxgig/sdkgen project)
 
--- ts/test/utility/StructUtility.test.ts
+++ ts/test/utility/StructUtility.test.ts
@@ -1,4 +1,4 @@
-// VERSION: @voxgig/struct 0.0.4
+// VERSION: @voxgig/struct 0.0.7
 // RUN: npm test
 // RUN-SOME: npm run test-some --pattern=getpath
 
